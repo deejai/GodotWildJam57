b4be86c9ce3a8ae56c664b916d3410c833de0698
deejai <deejai@live.com>
2023-05-21 13:37:56 -0600

    pressureplatewalls

--- Game/Characters/Player/HUD.gd
+++ Game/Characters/Player/HUD.gd
@@ -4,8 +4,13 @@ class_name PlayerHUD
 
 @onready var pickup_label: Label = $PickupLabel
 
+var show_help: bool = true
+
 func _ready():
 	pass
 
 func _process(delta):
-	pass
+	if Input.is_action_just_pressed("ToggleHelp"):
+		show_help = !show_help
+		$HelpText.visible = show_help
+		$HelpTextHint.visible = !show_help
